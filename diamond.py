79c589e9fec8f287be511db59b82a018e504f173
Dave Sizer <dasizer@gmail.com>
2015-03-05 13:51:01 -0500

    Python script that makes diamonds

---
+++ diamond.py
@@ -0,0 +1,28 @@
+__author__ = 'e305698'
+
+while 1==1:
+    try:
+        WIDTH = int(raw_input('Width?'))
+    except ValueError:
+        print 'Not a number!'
+        continue
+
+    CHAR = raw_input('Draw character?')
+
+    for i in range(1,WIDTH-1):
+        for j in range(1, WIDTH-i):
+            print(''),
+        for k in range(1,i):
+            print (CHAR),
+        print ''
+
+    for i in reversed(range(1,WIDTH)):
+        for j in range(1, WIDTH-i):
+            print(''),
+        for k in range(1,i):
+            print (CHAR),
+        print ''
+
+
+
+
